b9bc5460815c39cb4e06563fba2d9593f1f18608
vijayfromnamakkal <vijayfromnamakkal@gmail.com>
2020-08-30 10:07:12 +0530

    update

---
+++ client/src/Fib.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
+
+  componentDidMount() {
+     this.fetchValues();
+     this.fetchIndexes();
+   }
+
+   async fetchValues() {
+     const values = await axios.get('/api/values/current');
+     this.setState({ values: values.data });
+   }
+
+
+   async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+}
+
+render() {
+  return (
+    <div>
+      <form>
+        <label>Enter your index:</label>
+        <input />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+}
+
+export default Fib;
